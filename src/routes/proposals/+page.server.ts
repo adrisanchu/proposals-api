fc4c407a85ecc9a496fd28416344aeaa76349a22
adrisanchu <adrian.sanchez@ree.es>
2024-04-11 09:14:55 +0200

    (WIP) update database when clicking on the buttons + small changes on the layout

--- src/routes/proposals/+page.server.ts
+++ src/routes/proposals/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 import type { ProposalWithUser } from '$lib/types/Proposal';
+import type { Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	const proposals: ProposalWithUser[] = await prisma.proposal.findMany({
@@ -12,3 +13,9 @@ export const load: PageServerLoad = async () => {
 		proposals,
 	};
 };
+
+export const actions: Actions = {
+	checkProject: async () => {
+		console.log('checkProject');
+	}
+}
--- src/routes/proposals/+page.svelte
+++ src/routes/proposals/+page.svelte
@@ -1,65 +1,71 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import type { ProposalWithUser } from '$lib/types/Proposal';
+  import type { PageData } from "./$types";
+  import type { ProposalWithUser } from "$lib/types/Proposal";
 
-	export let data: PageData;
-	let proposals: ProposalWithUser[] = data.proposals;
+  export let data: PageData;
+  let proposals: ProposalWithUser[] = data.proposals;
 </script>
 
 <div class="container h-full mx-auto">
-	<h2 class="h2 py-4 mb-4">Propuestas</h2>
+  <h2 class="h2 py-4 mb-4">Propuestas</h2>
 
-	<div class="table-container space-y-4">
-		<table class="table table-hover table-compact table-auto w-full">
-			<thead>
-				<tr>
-					<td>Revisado?</td>
-					<td>ID</td>
-					<td>Código</td>
-					<td>Título</td>
-					<td>Nombre Usuario</td>
-					<td>Email</td>
-					<td></td>
-				</tr>
-			</thead>
-			<tbody>
-				{#each proposals as proposal (proposal.key)}
-					<tr>
-						<td>
-							<input
-								type="checkbox"
-								class="checkbox"
-								name=""
-								id=""
-								checked={proposal.checked}
-								on:click={() =>
-									console.log(
-										'status: ',
-										(proposal.checked = !proposal.checked)
-									)}
-							/>
-							<!-- <select class="select border-0 width-full leading-4">
+  <div class="table-container space-y-4">
+    <table class="table table-hover table-compact table-auto w-full">
+      <thead>
+        <tr>
+          <td>Acciones</td>
+          <td>Revisado?</td>
+          <td>ID</td>
+          <td>Código</td>
+          <td>Título</td>
+          <td>Nombre Usuario</td>
+          <td>Email</td>
+        </tr>
+      </thead>
+      <tbody>
+        {#each proposals as proposal (proposal.key)}
+          <tr>
+            <td class="flex space-x-4">
+              <button title="Detalles" on:click={() => console.log("detalles")}
+                >🔎</button
+              >
+              <button
+                title="Descartar"
+                on:click={() => console.log("descartar")}>❌</button
+              >
+            </td>
+            <td>
+              <form
+                method="POST"
+                action="?/checkProposal&id={proposal.key}&checked={proposal.checked}"
+              >
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  name=""
+                  id=""
+                  checked={proposal.checked}
+                  on:click={() =>
+                    console.log(
+                      "status: ",
+                      (proposal.checked = !proposal.checked)
+                    )}
+                />
+              </form>
+
+              <!-- <select class="select border-0 width-full leading-4">
 								<option value="true">✅</option>
 								<option value="true">❌</option>
 							</select> -->
-						</td>
-						<td>{proposal.key}</td>
-						<td class="align-middle">{proposal.code}</td>
-						<td>{proposal.title}</td>
-						<td>{proposal.user.name}</td>
-						<td>{proposal.user.email}</td>
-						<td class="space-x-4">
-							<button title="Detalles" on:click={() => console.log('detalles')}
-								>🔎</button
-							>
-							<button
-								title="Descartar"
-								on:click={() => console.log('descartar')}>❌</button
-							>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
+            </td>
+            <td>{proposal.id}</td>
+            <td class="align-middle">{proposal.code}</td>
+            <td>{proposal.title}</td>
+            <td>{proposal.user.name}</td>
+            <td>{proposal.user.email}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
